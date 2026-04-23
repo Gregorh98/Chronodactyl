b88129c28311f4f1f10324bae9d4a0a055b7a4ed
Gregorh98 <33038097+Gregorh98@users.noreply.github.com>
2026-04-23 10:13:56 +0100

    Removed unused variable

--- src/Chronodactyl.cpp
+++ src/Chronodactyl.cpp
@@ -30,7 +30,7 @@ void update_time()
     hour_value = (hour_value + 1) % 24;
   }
 
-  int8_t minute_value = now.minute();
+  //int8_t minute_value = now.minute();
   //int8_t second_value = now.second();
 
   hand.show_binary(hour_value);
